247a402d105b08e2a770fd8a195fd6e09e527c5d
josebrianz <brianzjose@gmail.com>
2026-07-03 19:09:57 +0300

    Predicted customer churn with temp data.

---
+++ numarr.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr1 = np.array([10,20,89])
+print (arr1)
+
+arr2 = np.array([[1,2],[3,5],[7,39]])
+print(arr2)
+
+for array in arr1:
+    print(arr1[1])
+
+for array in arr2:
+    print(array[0])
